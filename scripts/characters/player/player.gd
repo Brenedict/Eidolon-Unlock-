f051effae514b38d6d68a94c89f2df6ee38c23b3
ken85illo <pabillokenneth81@gmail.com>
2025-09-29 12:57:29 +0800

    feat: implement basic platformer physics and animations

---
+++ scripts/characters/player/player.gd
@@ -0,0 +1,62 @@
+class_name Player
+extends PlatformerCharacter2D
+
+@export var idle_timer : Timer
+@export_range(0, 300, 0.2, "or_greater") var walk_speed : float = 75.0
+@export_range(0, 300, 0.2, "or_greater") var run_speed : float = 150.0
+@export_range(0, 300, 0.2, "or_greater") var jump_force : float = 250.0
+var run : bool = false
+
+func _ready():
+	animated_sprite.play(animations.idle_1)
+	idle_timer.start()
+
+func _physics_process(delta: float) -> void:
+	var move_speed = run_speed if run else walk_speed
+	velocity.x = direction.x * move_speed
+	_apply_gravity(delta)
+
+	move_and_slide()
+	process_animations()
+
+func process_animations() -> void:
+	if not is_on_floor():
+		return
+
+	if abs(direction.x) > 0.0:
+		play_animation(animations.run if run else animations.walk)
+		_reset_idle_timer()
+		return
+
+	_handle_idle_animation()
+
+func _reset_idle_timer():
+	if !idle_timer.paused:
+		idle_timer.start()
+		idle_timer.paused = true
+
+func _handle_idle_animation():
+	if current_animation() == animations.idle_2:
+		return
+
+	play_animation(animations.idle_1)
+	if idle_timer.time_left <= 0:
+		play_animation(animations.idle_2)
+	elif idle_timer.paused or idle_timer.is_stopped():
+		idle_timer.paused = false
+		idle_timer.start()
+
+
+# Makes the character jump if possible
+func try_jump() -> bool:
+	if is_on_floor():
+		_jump()
+		return true
+	
+	return false
+
+# Execute a ground jump
+func _jump() -> void:
+	velocity.y -= jump_force
+	stop_animation()
+	play_animation(animations.jump)
---
+++ scripts/characters/player/player_input.gd
@@ -0,0 +1,14 @@
+class_name PlayerInput
+extends Node
+
+@export var player : Player
+@export var actions : PlayerInputActions
+
+func _process(_delta: float) -> void:
+	player.direction = Input.get_vector(actions.left, actions.right, actions.up, actions.down)
+
+func _unhandled_input(event: InputEvent) -> void:
+	player.run = Input.is_action_pressed("run")
+
+	if Input.is_action_pressed("jump"):
+		player.try_jump()
